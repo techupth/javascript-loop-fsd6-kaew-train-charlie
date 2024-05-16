16ae084a5378b23fd06641801d6016b6ff636509
CharlieHii <charlie_chaper@hotmail.com>
2024-05-16 12:32:48 +0700

    feat(*.js): complete all assignments

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,8 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let brand of restaurants) {
+  newRestaurants.push(`Restaurant Name: ${brand}`);
+}
 
 console.log(newRestaurants);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let char of companyName) {
+  reversedCompanyName = char + reversedCompanyName;
+}
 
 console.log(reversedCompanyName);
